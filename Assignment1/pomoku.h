397d3458349ad2f49deefeaf66dede922af9ae6d
JEL97 <gh243700@gmail.com>
2022-05-03 14:09:33 +0900

    bak01

---
+++ Assignment1/pomoku.h
@@ -0,0 +1,43 @@
+#include "color.h"
+
+#ifndef POMOKU_H
+#define POMOKU_H
+
+void init_game(void);
+
+unsigned int get_row_count(void);
+
+unsigned int get_column_count(void);
+
+int get_score(const color_t color);
+
+int get_color(const unsigned int row, const unsigned int col);
+
+int is_placeable(const unsigned int row, const unsigned int col);
+
+int place_stone(const color_t color, const unsigned int row, const unsigned int col);
+
+/* special moves */
+int insert_row(const color_t color, const unsigned int row);
+
+int insert_column(const color_t color, const unsigned int col);
+
+int remove_row(const color_t color, const unsigned int row);
+
+int remove_column(const color_t color, const unsigned int col);
+
+int swap_rows(const color_t color, const unsigned int row0, const unsigned int row1);
+
+int swap_columns(const color_t color, const unsigned int col0, const unsigned int col1);
+
+int copy_row(const color_t color, const unsigned int src, const unsigned int dst);
+
+int copy_column(const color_t color, const unsigned int src, const unsigned int dst);
+
+void print_full_board(void);
+
+
+
+int get_stone_count(const color_t color, const unsigned int row, const unsigned int col, const int row_increment, const int col_increment);
+
+#endif
