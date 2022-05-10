fdb8f9f65e340a326a9ac4e25c37da1ef987cd3b
JEL97 <gh243700@gmail.com>
2022-05-10 18:42:53 +0900

    lab01_bak01

---
+++ Lab2/array.c
@@ -0,0 +1,140 @@
+#include "array.h"
+#include <limits.h>
+
+int get_index_of(const int numbers[], const size_t element_count, const int num) {
+    int i;
+    const int* ptr = numbers;
+    
+    for(i = 0; i < element_count; i++) {
+        if(*ptr++ == num) {
+            return i;
+        }
+    }
+    
+    return -1;
+}
+
+
+int get_last_index_of(const int numbers[], const size_t element_count, const int num) {
+    
+    int i; 
+    const int* ptr = numbers + element_count - 1;
+    
+    for(i = 0; i < element_count; i++) {
+        if (*ptr == num) {
+            return element_count - i - 1;
+        }
+        ptr--;
+    }
+
+    return -1;
+}
+
+int get_max_index(const int numbers[], const size_t element_count) {
+    int i; 
+    const int* max_ptr; 
+    const int* ptr = numbers;
+
+    if (element_count == 0) {
+        return -1;
+    }
+
+    max_ptr = numbers;
+    ptr++;    
+
+   for(i = 0; i < element_count; i++) {
+        if(*ptr > *max_ptr) { 
+            max_ptr = ptr; 
+        }
+        ptr++;
+    }
+
+    return max_ptr - numbers;
+}
+
+int get_min_index(const int numbers[], const size_t element_count) {
+    int i; 
+    const int* min_ptr; 
+    const int* ptr = numbers;
+
+    if (element_count == 0) {
+        return -1;
+    }
+
+    min_ptr = numbers;  
+
+   for(i = 0; i < element_count; i++) {
+        if(*ptr < *min_ptr) { 
+            min_ptr = ptr; 
+        }
+        ptr++;
+    }
+    return min_ptr - numbers;
+}
+
+int is_all_positive(const int numbers[], const size_t element_count) {
+    int i;
+    const int* ptr = numbers;
+
+    if(element_count == 0) {
+        return FALSE;
+    }
+
+    for(i = 0; i < element_count; i++) {
+        if(*ptr++ < 0) { 
+            return FALSE;
+        }
+    }        
+
+    return TRUE;
+}
+
+int has_even(const int numbers[], const size_t element_count) {
+    int i;   
+    const int* ptr = numbers;
+  
+    for(i = 0; i < element_count; i++) {
+        if(*ptr++ % 2 == 0) {
+            return TRUE;
+        }            
+    }
+
+    return FALSE;
+}
+
+int insert(int numbers[], const size_t element_count, const int num, const size_t pos) {
+    int i; 
+    int* ptr = numbers;
+
+    if(pos > element_count) {
+        return FALSE;
+    }
+
+    for(i = element_count - 1; i > pos; i--) {
+        *(ptr + i + 1) = *(ptr + i);
+        ptr++;
+    }
+    ptr[pos] = num;
+
+    return TRUE;
+}
+
+int remove_at(int numbers[], const size_t element_count, const size_t index) {
+    int i;
+    int* ptr = numbers;
+
+    if(element_count < index) {
+        return FALSE;
+    }
+
+    for( i = index; i < element_count - 1; i++) {
+        *(ptr + i) = *(ptr + i + 1);
+        ptr++;
+    } 
+    *ptr = INT_MIN;
+
+    return TRUE;
+}
+
+
+
--- Lab2/main.c
+++ Lab2/main.c
@@ -1,4 +1,43 @@
+#include <assert.h>
+#include <limits.h>
+#include <stdio.h>
+
+#include "array.h"
+
 int main(void)
 {
+{
+    int ary[7] = { 1, 4, 5, 6, 6, INT_MIN, INT_MIN };
+    assert(get_index_of(ary, 5, 6) == 3); /* 3 */
+    assert(get_index_of(ary, 5, 2) == -1); /* -1 */
+
+
+}
+{
+    int ary[7] = { 1, 4, 6, 6, 5, INT_MIN, INT_MIN };
+    assert(get_last_index_of(ary, 5, 6) == 3); /* 3 */
+    printf("%d sss\n", get_last_index_of(ary, 5, 2));
+    assert(get_last_index_of(ary, 5, 2) == -1); /* -1 */
+}
+{
+    int ary1[7] = { 1, 4, 6, 6, 5, INT_MIN, INT_MIN };
+    int ary2[0];
+    assert(get_max_index(ary1, 5) == 2); /* 2 */
+    printf("%d\n", get_max_index(ary2, 0));
+    assert(get_max_index(ary2, 0) == -1); /* -1 */
+    
+    
+}
+{
+    int ary1[7] = { 1, 4, -6, -6, 5, INT_MIN, INT_MIN };
+    int ary2[0];
+    assert(get_min_index(ary1, 5) == 2); /* 2 */
+    assert(get_min_index(ary2, 0) == -1); /* -1 */
+
+}
+
+
+    printf("test finsssss\n");
     return 0;
+
 }
