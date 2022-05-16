9b6c2e517e92f150cff8e25988e0d8376509904c
JEL97 <gh243700@gmail.com>
2022-05-16 18:34:16 +0900

    init_lab3

---
+++ Lab3/cyber_spaceship.c
@@ -0,0 +1,84 @@
+#include "cyber_spaceship.h"
+
+const char* get_longest_safe_zone_or_null(const char* const cab_start_location, const size_t cab_length, const char* const cluster_start_locations[], const size_t cluster_lengths[], const size_t cluster_count, size_t* out_longest_safe_area_length)
+{
+    const char* cab_location_ptr = cab_start_location;
+    size_t i; 
+    size_t j;  
+    const char* safe_zone_start_ptr = (void*) 0;
+    const char* safe_zone_start_temp_ptr = (void*) 0;
+    size_t safe_zone_count_temp = 0;
+
+    *out_longest_safe_area_length = 0;
+
+    if (cab_length == 0) {
+        return (void*) 0;
+    }
+
+    if (cluster_count == 0) {
+        *out_longest_safe_area_length = cab_length;
+        return cab_start_location;
+    }
+    
+    for (i = 0; i < cab_length; i++) {
+        size_t cluster_overlap = 0;
+        
+        for (j = 0; j < cluster_count; j++) {
+            const char* cluster_ptr = cluster_start_locations[j];
+            const size_t cluster_length = cluster_lengths[j]; 
+            if (cab_location_ptr >= cluster_ptr && (cab_location_ptr < cluster_ptr + cluster_length)) {
+                cluster_overlap++;    
+            }
+        }
+
+        if (cluster_overlap % 2 == 0) {
+            if (safe_zone_count_temp == 0) {
+                safe_zone_start_temp_ptr = cab_location_ptr;
+            }
+            safe_zone_count_temp++;
+            if (i == cab_length - 1) {
+                goto label1;
+            }
+        } else {
+label1:
+            if (*out_longest_safe_area_length <= safe_zone_count_temp) {
+                safe_zone_start_ptr = safe_zone_start_temp_ptr;
+                *out_longest_safe_area_length = safe_zone_count_temp;
+            }
+            safe_zone_count_temp = 0;
+        }
+        cab_location_ptr++;
+    }
+
+     return safe_zone_start_ptr;
+}
+
+int get_travel_time(const char* const cab_start_location, const size_t cab_length, const char* const cluster_start_locations[], const size_t cluster_lengths[], const size_t cluster_count)
+{
+    const char* cab_location_ptr = cab_start_location;
+    size_t i;
+    size_t j;
+    double travel_time = 0;
+    
+    for (i = 0; i < cab_length; i++) {
+        
+        size_t overlap_count = 0;   
+        const size_t* cluster_length_ptr = cluster_lengths;
+        for (j = 0; j < cluster_count; j++) {
+            const char* cluster_start_location = cluster_start_locations[j];    
+            if (cab_location_ptr >= cluster_start_location && cab_location_ptr < cluster_start_location + *cluster_length_ptr) {
+                    overlap_count++;
+                }
+            cluster_length_ptr++; 
+        }
+        
+        if (overlap_count % 2 == 1) {
+            travel_time += 0.2;
+        } else {
+            travel_time += 0.1;
+        }
+        cab_location_ptr++;
+    }
+    
+    return (int)(travel_time + 0.5);
+}
--- Lab3/main.c
+++ Lab3/main.c
@@ -1,4 +1,148 @@
+#include "cyber_spaceship.h"
+#include <assert.h>
+#include <stdio.h>
+
+#define CAB_LENGTH (50)
 int main(void)
 {
-    return 0;
+{
+    const char cyber_asteroid_belt[30];
+    const char* cluster_start_addresses[2];
+    size_t cluster_lengths[2];
+    size_t out_longest_safe_area_length = 0;
+    size_t* out_longest_safe_area_length_p = &out_longest_safe_area_length;
+    const char* longest_safe_cluster_start_address;
+
+    const char* cluster1 = &cyber_asteroid_belt[12];
+    const char* cluster2 = &cyber_asteroid_belt[3];
+
+    cluster_start_addresses[0] = cluster1;
+    cluster_start_addresses[1] = cluster2;
+
+    cluster_lengths[0] = 15U;
+    cluster_lengths[1] = 25U;
+
+    longest_safe_cluster_start_address = get_longest_safe_zone_or_null(cyber_asteroid_belt, 30, cluster_start_addresses, cluster_lengths, 2, out_longest_safe_area_length_p); /* longest_safe_cluster_start_address: (cyber_asteroid_belt + 12), out_longest_safe_area_length: 15 */
+    printf("%d\n", longest_safe_cluster_start_address - cyber_asteroid_belt);
+    printf("%d\n", out_longest_safe_area_length);
+    assert(longest_safe_cluster_start_address == cyber_asteroid_belt + 12);
+    assert(out_longest_safe_area_length == 15);
+} 
+
+{
+    const char cyber_asteroid_belt[30];
+    const char* cluster_start_addresses[1];
+    size_t cluster_lengths[1];
+    size_t out_longest_safe_area_length = 0;
+    size_t* out_longest_safe_area_length_p = &out_longest_safe_area_length;
+    const char* longest_safe_cluster_start_address;
+    
+    const char* cluster1 = &cyber_asteroid_belt[0];
+    cluster_start_addresses[0] = cluster1;
+    cluster_lengths[0] = 26U;
+
+    longest_safe_cluster_start_address = get_longest_safe_zone_or_null(cyber_asteroid_belt, 30, cluster_start_addresses, cluster_lengths, 1, out_longest_safe_area_length_p); /* longest_safe_cluster_start_address: (cyber_asteroid_belt + 12), out_longest_safe_area_length: 15 */
+    printf("sss is %d\n", out_longest_safe_area_length);
+    assert(out_longest_safe_area_length == 4);
+    assert(longest_safe_cluster_start_address == cyber_asteroid_belt + 26);
+}
+
+{
+    const char cyber_asteroid_belt[30];
+    const char* cluster_start_addresses[2];
+    size_t cluster_lengths[2];
+    size_t out_longest_safe_area_length = 0;
+    size_t* out_longest_safe_area_length_p = &out_longest_safe_area_length;
+    const char* longest_safe_cluster_start_address;
+
+    const char* cluster1 = &cyber_asteroid_belt[5];
+    const char* cluster2 = &cyber_asteroid_belt[15];
+
+    cluster_start_addresses[0] = cluster1;
+    cluster_start_addresses[1] = cluster2;
+
+    cluster_lengths[0] = 10U;
+    cluster_lengths[1] = 10U;
+
+    longest_safe_cluster_start_address = get_longest_safe_zone_or_null(cyber_asteroid_belt, 30, cluster_start_addresses, cluster_lengths, 2, out_longest_safe_area_length_p); /* longest_safe_cluster_start_address: (cyber_asteroid_belt + 12), out_longest_safe_area_length: 15 */
+    printf("%d\n", longest_safe_cluster_start_address - cyber_asteroid_belt);
+    printf("%d\n", out_longest_safe_area_length);
+    assert(longest_safe_cluster_start_address == cyber_asteroid_belt + 25);
+    assert(out_longest_safe_area_length == 5);
+}
+
+
+{
+const char cyber_asteroid_belt[30];
+const char* cluster_start_addresses[2];
+size_t cluster_lengths[2];
+int time_in_mins;
+
+const char* cluster1 = &cyber_asteroid_belt[12];
+const char* cluster2 = &cyber_asteroid_belt[3];
+
+cluster_start_addresses[0] = cluster1;
+cluster_start_addresses[1] = cluster2;
+
+cluster_lengths[0] = 15U;
+cluster_lengths[1] = 25U;
+
+time_in_mins = get_travel_time(cyber_asteroid_belt, 30, cluster_start_addresses, cluster_lengths, 2); /* 4 */
+
+
+assert(time_in_mins == 4);
+
+
+}
+{
+    char cab[CAB_LENGTH];
+    const char* cluster_start_addresses[5];
+    size_t cluster_lengths[5];
+
+    size_t out_longest_safe_area_length = 0;
+    size_t* out_longest_safe_area_length_p = &out_longest_safe_area_length;
+    const char* longest_safe_cluster_start_address;
+    int time_in_mins;
+    const char* cab_start_address = cab;
+
+    cluster_start_addresses[0] = &cab[8];
+    cluster_start_addresses[1] = &cab[1];
+    cluster_start_addresses[2] = &cab[13];
+    cluster_start_addresses[3] = &cab[33];
+    cluster_start_addresses[4] = &cab[38];
+
+    cluster_lengths[0] = 10U;
+    cluster_lengths[1] = 22U;
+    cluster_lengths[2] = 10U;
+    cluster_lengths[3] = 13U;
+    cluster_lengths[4] = 10U;
+
+    longest_safe_cluster_start_address = get_longest_safe_zone_or_null(cab_start_address, CAB_LENGTH, cluster_start_addresses, cluster_lengths, 5, out_longest_safe_area_length_p);
+
+    assert(out_longest_safe_area_length == 15);
+    assert(longest_safe_cluster_start_address == cab_start_address + 18);
+
+    time_in_mins = get_travel_time(cab_start_address, CAB_LENGTH, cluster_start_addresses, cluster_lengths, 5);
+    printf("times in min %d\n", time_in_mins);
+    assert(time_in_mins == 7);
+}
+
+
+{
+const char cyber_asteroid_belt[30];
+int time_in_mins;
+
+time_in_mins = get_travel_time(cyber_asteroid_belt, 30, (void*)0, (void*)0, 0); /* 4 */
+
+
+assert(time_in_mins == 3);
+
+
+
+
+
+}
+
+   printf("fin\n");
+   return 0;
 }
