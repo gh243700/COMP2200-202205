2c97aa05072612b62a9b95ceb0fefd5396b85a7d
JEL97 <gh243700@gmail.com>
2022-05-16 19:19:38 +0900

    Update cyber_spaceship.c
    fix get_travel_time

--- Lab3/cyber_spaceship.c
+++ Lab3/cyber_spaceship.c
@@ -1,5 +1,6 @@
 #include "cyber_spaceship.h"
 
+
 const char* get_longest_safe_zone_or_null(const char* const cab_start_location, const size_t cab_length, const char* const cluster_start_locations[], const size_t cluster_lengths[], const size_t cluster_count, size_t* out_longest_safe_area_length)
 {
     const char* cab_location_ptr = cab_start_location;
@@ -59,9 +60,14 @@ int get_travel_time(const char* const cab_start_location, const size_t cab_lengt
     size_t i;
     size_t j;
     double travel_time = 0;
-    
+    size_t zone_count = 0;
+    int is_safe = -1;    
+
+    if (cluster_count == 0) { 
+        return cab_length / 10;
+    }
+
     for (i = 0; i < cab_length; i++) {
-        
         size_t overlap_count = 0;   
         const size_t* cluster_length_ptr = cluster_lengths;
         for (j = 0; j < cluster_count; j++) {
@@ -71,14 +77,43 @@ int get_travel_time(const char* const cab_start_location, const size_t cab_lengt
                 }
             cluster_length_ptr++; 
         }
-        
-        if (overlap_count % 2 == 1) {
-            travel_time += 0.2;
-        } else {
-            travel_time += 0.1;
+
+        if (is_safe == -1) {
+            is_safe = (overlap_count % 2) ? 0 : 1;
+            goto label3;
         }
+    
+        if (i == cab_length - 1) {
+            if (is_safe) {
+                goto label1;
+            } else {
+                goto label2;
+            }
+        }
+
+        if (overlap_count % 2) {
+label1:
+            if (is_safe) {
+                travel_time += zone_count / 5.0;
+                printf("safezone : %d\n", zone_count);
+                zone_count = 0;
+            }
+            is_safe = 0;
+        } else {
+label2:
+            if (!is_safe) {
+                travel_time += zone_count / 10.0;
+                printf("danger : %d\n", zone_count);
+                zone_count = 0;
+            }
+            is_safe = 1;
+        }
+label3:
+        zone_count++;        
         cab_location_ptr++;
     }
+
+    printf("%f\n", travel_time);
     
-    return (int)(travel_time + 0.5);
+    return (int)(travel_time);
 }
