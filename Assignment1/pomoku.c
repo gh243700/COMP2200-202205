1dd5541ab6dca2a3f33b59521adf6a5545af5f28
JEL97 <gh243700@gmail.com>
2022-05-17 18:59:07 +0900

    Update pomoku.c

--- Assignment1/pomoku.c
+++ Assignment1/pomoku.c
@@ -7,12 +7,12 @@
 #define MAX_COL (20)
 #define MIN_COL (10)
 
-int board[20][20];
-int row_count;
-int col_count;
+static int s_board[20][20];
+static int s_row_count;
+static int s_col_count;
 
-int black_stone_score;
-int white_stone_score;
+static int s_black_stone_score;
+static int s_white_stone_score;
 
 int is_valid_scope(const int row, const int col);
 int* get_stone_score(const color_t color);
@@ -21,13 +21,13 @@ int get_stone_count(const color_t color, const unsigned int row, const unsigned 
 void init_game(void)
 {
     int i;
-    int* p = board[0];
+    int* p = s_board[0];
     
-    col_count = 15;
-    row_count = 15;
+    s_col_count = 15;
+    s_row_count = 15;
     
-    black_stone_score = 0;
-    white_stone_score = 0;
+    s_black_stone_score = 0;
+    s_white_stone_score = 0;
     
     for (i = 0; i < MAX_ROW * MAX_COL; i++) {
         *p++ = -1;
@@ -37,12 +37,12 @@ void init_game(void)
 
 unsigned int get_row_count(void) 
 {
-    return row_count;
+    return s_row_count;
 }
 
 unsigned int get_column_count(void) 
 {
-    return col_count;
+    return s_col_count;
 }
 
 int get_score(const color_t color)
@@ -60,12 +60,12 @@ int get_color(const unsigned int row, const unsigned int col)
     if (!is_valid_scope(row, col)) {
         return -1;
     }
-    return board[row][col];
+    return s_board[row][col];
 }
 
 int is_placeable(const unsigned int row, const unsigned int col) 
 {
-    if (!is_valid_scope(row, col) || board[row][col] != -1) {
+    if (!is_valid_scope(row, col) || s_board[row][col] != -1) {
         return 0;
     }
     
@@ -82,7 +82,7 @@ int place_stone(const color_t color, const unsigned int row, const unsigned int 
     }
     
     stone_score = get_stone_score(color);
-    board[row][col] = color;
+    s_board[row][col] = color;
     
     stone_count = get_stone_count(color, row + 1, col + 1, 1, 1) + get_stone_count(color, row - 1, col - 1, -1, -1) + 1;
     
@@ -119,19 +119,19 @@ int insert_row(const color_t color, const unsigned int row)
     int j;
     int* score = get_stone_score(color);
     
-    if (score == NULL || *score < 3 || row < 0 || row > row_count || row_count >= MAX_ROW) {
+    if (score == NULL || *score < 3 || row < 0 || row > s_row_count || s_row_count >= MAX_ROW) {
         return 0;
     }
     *score -= 3;
     
-    for (i = row_count - 1; i >= (int)row; i--) {
-        for (j = 0; j < col_count; j++) {    
-            board[i + 1][j] = board[i][j];
-            board[i][j] = -1;
+    for (i = s_row_count - 1; i >= (int)row; i--) {
+        for (j = 0; j < s_col_count; j++) {    
+            s_board[i + 1][j] = s_board[i][j];
+            s_board[i][j] = -1;
         }
     }    
 
-    row_count++;
+    s_row_count++;
 
     return 1;
 }
@@ -143,20 +143,20 @@ int insert_column(const color_t color, const unsigned int col)
     int j;
     int* score = get_stone_score(color);
     
-    if (score == NULL || *score < 3 || col < 0 || col > col_count || col_count >= MAX_COL) {
+    if (score == NULL || *score < 3 || col < 0 || col > s_col_count || s_col_count >= MAX_COL) {
         return 0;
     }
 
     *score -= 3;
 
-    for (i = 0; i < row_count; i++) {
-        for (j = col_count - 1; j >= (int)col; j--) {    
-            board[i][j + 1] = board[i][j];
-            board[i][j] = -1;
+    for (i = 0; i < s_row_count; i++) {
+        for (j = s_col_count - 1; j >= (int)col; j--) {    
+            s_board[i][j + 1] = s_board[i][j];
+            s_board[i][j] = -1;
         }
     }    
 
-    col_count++;
+    s_col_count++;
 
     return 1;
 }
@@ -167,19 +167,19 @@ int remove_row(const color_t color, const unsigned int row)
     int i;
     int j;
 
-    if (score == NULL || *score < 3 || row >= row_count || row_count <= MIN_ROW) {
+    if (score == NULL || *score < 3 || row >= s_row_count || s_row_count <= MIN_ROW) {
         return 0;
     }
 
     *score -= 3;
 
-    for (i = row; i < row_count; i++) {
-        for (j = 0; j < col_count; j++) {
-            board[i][j] = board[i + 1][j];
+    for (i = row; i < s_row_count; i++) {
+        for (j = 0; j < s_col_count; j++) {
+            s_board[i][j] = s_board[i + 1][j];
         }        
     }
         
-    row_count--;    
+    s_row_count--;    
 
     return 1;
 }
@@ -190,19 +190,19 @@ int remove_column(const color_t color, const unsigned int col)
     int i;
     int j;
 
-    if (score == NULL || *score < 3 || col >= col_count || col_count <= MIN_COL) {
+    if (score == NULL || *score < 3 || col >= s_col_count || s_col_count <= MIN_COL) {
         return 0;
     }
 
    *score -= 3;
 
-    for (i = 0; i < row_count; i++) {
-        for (j = col; j < col_count; j++) {
-            board[i][j] = board[i][j + 1];
+    for (i = 0; i < s_row_count; i++) {
+        for (j = col; j < s_col_count; j++) {
+            s_board[i][j] = s_board[i][j + 1];
         }        
     }
         
-    col_count--;
+    s_col_count--;
 
     return 1;
 }
@@ -210,17 +210,17 @@ int remove_column(const color_t color, const unsigned int col)
 int swap_rows(const color_t color, const unsigned int row0, const unsigned int row1)
 {
     int i; 
-    int* ptr1 = board[row0];
-    int* ptr2 = board[row1];    
+    int* ptr1 = s_board[row0];
+    int* ptr2 = s_board[row1];    
     int* score = get_stone_score(color);
 
-    if (score == NULL || *score < 2 || row0 >= row_count || row1 >= row_count) {
+    if (score == NULL || *score < 2 || row0 >= s_row_count || row1 >= s_row_count) {
         return 0;
     }
 
     *score -= 2;
 
-    for (i = 0; i < row_count; i++) {
+    for (i = 0; i < s_row_count; i++) {
         *ptr1 ^= *ptr2;
         *ptr2 ^= *ptr1;
         *ptr1 ^= *ptr2;
@@ -234,17 +234,17 @@ int swap_rows(const color_t color, const unsigned int row0, const unsigned int r
 int swap_columns(const color_t color, const unsigned int col0, const unsigned int col1) 
 {
     int i;
-    int* ptr1 = &board[0][col0];
-    int* ptr2 = &board[0][col1];
+    int* ptr1 = &s_board[0][col0];
+    int* ptr2 = &s_board[0][col1];
     int* score = get_stone_score(color);
 
-    if (score == NULL || *score < 2 || col0 >= col_count || col1 >= col_count) {
+    if (score == NULL || *score < 2 || col0 >= s_col_count || col1 >= s_col_count) {
         return 0;
     }
 
     *score -= 2;
 
-    for (i = 0; i < col_count; i++) {
+    for (i = 0; i < s_col_count; i++) {
         *ptr1 ^= *ptr2;
         *ptr2 ^= *ptr1;
         *ptr1 ^= *ptr2;
@@ -258,17 +258,17 @@ int swap_columns(const color_t color, const unsigned int col0, const unsigned in
 int copy_row(const color_t color, const unsigned int src, const unsigned int dst) 
 {
     int i; 
-    int* ptr1 = board[src];
-    int* ptr2 = board[dst];
+    int* ptr1 = s_board[src];
+    int* ptr2 = s_board[dst];
     int* score = get_stone_score(color);
 
-    if (score == NULL || *score < 4 || src >= row_count || dst >= row_count) {
+    if (score == NULL || *score < 4 || src >= s_row_count || dst >= s_row_count) {
         return 0;
     }
 
     *score -= 4;
 
-    for (i = 0; i < row_count; i++) {
+    for (i = 0; i < s_row_count; i++) {
         *ptr2++ = *ptr1++;
     }
 
@@ -278,17 +278,17 @@ int copy_row(const color_t color, const unsigned int src, const unsigned int dst
 int copy_column(const color_t color, const unsigned int src, const unsigned int dst)
 {
     int i;
-    int* ptr1 = &board[0][src];
-    int* ptr2 = &board[0][dst];
+    int* ptr1 = &s_board[0][src];
+    int* ptr2 = &s_board[0][dst];
     int* score = get_stone_score(color);
 
-    if (score == NULL || *score < 4 || src >= col_count || dst >= col_count) {
+    if (score == NULL || *score < 4 || src >= s_col_count || dst >= s_col_count) {
         return 0;
     }
 
     *score -= 4;
 
-    for (i = 0; i < col_count; i++) {
+    for (i = 0; i < s_col_count; i++) {
         *ptr2 = *ptr1;
         ptr1 += MAX_COL;
         ptr2 += MAX_COL; 
@@ -302,10 +302,10 @@ int* get_stone_score(const color_t color) {
     
     switch (color) {
     case COLOR_BLACK: 
-        score = &black_stone_score;
+        score = &s_black_stone_score;
     break;
 	case COLOR_WHITE: 
-        score = &white_stone_score;
+        score = &s_white_stone_score;
     break;
     default: 
         return NULL;
@@ -316,7 +316,7 @@ int* get_stone_score(const color_t color) {
 }
 
 int get_stone_count(const color_t color, const unsigned int row, const unsigned int col,const int row_increment,const int col_increment) {
-    if (!is_valid_scope(row, col) || board[row][col] != color) {
+    if (!is_valid_scope(row, col) || s_board[row][col] != color) {
         return 0;
     }
     
@@ -325,7 +325,7 @@ int get_stone_count(const color_t color, const unsigned int row, const unsigned 
 
 
 int is_valid_scope(const int r, const int c) {
-    if (-1 >= r || row_count <= r || -1 >= c || col_count <= c) {
+    if (-1 >= r || s_row_count <= r || -1 >= c || s_col_count <= c) {
         return 0;
     }
     
