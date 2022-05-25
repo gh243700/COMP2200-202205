7244ef4fd2e158b7b9866c460391e8b99f674da5
JEL97 <gh243700@gmail.com>
2022-05-25 15:29:46 +0900

    lab4_init

--- Lab4/main.c
+++ Lab4/main.c
@@ -1,4 +1,80 @@
+#include "my_string.h"
+#include <string.h>
+#include <assert.h>
+#include <stdio.h>
+
 int main(void)
 {
+
+    {
+       char a[3]; 
+       char* ptr = a;
+       a[0] = 'a';
+       a[1] = 'b';
+       a[2] = '\n';
+        
+
+       *ptr ^= *(ptr + 1);
+       *(ptr + 1) ^= *ptr;
+       *ptr ^= *(ptr + 1);
+
+    }
+
+    {
+         char str[12 + 1] = "Oh happy day";
+         char* expected = "yad yppah hO";
+         reverse(str);
+         assert(strcmp(str, expected) == 0);
+
+
+    }
+
+    {
+        char str[] = "Oh happy day";
+        int index = index_of(str, "happy"); /* index: 3 */
+        assert(index == 3);
+
+    }
+
+{
+char str[] = "Oh happy day";
+char* ex = "hO yppah yad";
+reverse_by_words(str); /* str이 "hO yppah yad"가 됨 */
+assert(strcmp(str, ex) == 0);
+
+}
+
+{
+char str[] = "Oh happy day! My lovely day!";
+printf("%s\n" ,tokenize(str, " !")); /* &str[0]을 반환. 토큰은 "Oh" */
+printf("%s\n" ,tokenize(NULL, " !"));
+printf("%s\n" ,tokenize(NULL, " !"));
+printf("%s\n" ,tokenize(NULL, " !"));
+printf("%s\n" ,tokenize(NULL, " !"));
+printf("%s\n" ,tokenize(NULL, " !"));
+printf("%s\n" ,tokenize(NULL, " !"));
+printf("%s\n" ,tokenize(NULL, " !"));
+}
+printf("\n");
+{
+
+
+char str[] = "Oh happy day! My lovely day!";
+printf("%s\n" ,reverse_tokenize(str, " !")); /* &str[0]을 반환. 토큰은 "hO" */
+printf("%s\n" ,reverse_tokenize(NULL, " !")); /* &str[3]을 반환. 토큰은 "yppah" */
+printf("%s\n" ,reverse_tokenize(NULL, " !")); /* &str[9]을 반환. 토큰은 "yad" */
+printf("%s\n" ,reverse_tokenize(NULL, " !")); /* &str[14]을 반환. 토큰은 "yM" */
+
+tokenize(NULL, " !"); /* &str[17]을 반환. 토큰은 "lovely" */
+reverse_tokenize(NULL, " !"); /* &str[24]을 반환. 토큰은 "yad" */
+
+
+
+
+}
+
+
+    
+    printf("test fin\n");
     return 0;
 }
---
+++ Lab4/my_string.c
@@ -0,0 +1,130 @@
+#include "my_string.h"
+#include <assert.h>
+
+static int length(const char* str);
+
+void reverse(char* str)
+{
+    int i; 
+    char* ptr = str;
+    int len = length(str);
+
+    for (i = 0; i < len / 2; i++) {
+        *ptr ^= *(str + len - i - 1);
+        *(str + len - i - 1) ^= *ptr;
+        *ptr ^= *(str + len - i - 1);
+         ptr++;
+    }
+}
+
+int index_of(const char* str, const char* word)
+{
+    const char* str_ptr = str;
+    while (*str_ptr != '\0') {
+        const char* word_ptr = word;
+        const char* str_ptr_temp = str_ptr;
+        while (*str_ptr_temp == *word_ptr) {
+            str_ptr_temp++;
+            word_ptr++;
+	    if (*word_ptr == '\0') {
+                return str_ptr - str;
+            }
+        }
+        str_ptr++;
+    }
+    return -1;
+}
+
+void reverse_by_words(char* str)
+{
+    char* str_ptr0 = str;
+    char* str_ptr1 = str;
+    while (*str_ptr1 != '\0') {
+        if (*(str_ptr1 + 1) == '\0') {
+            reverse(str_ptr0);
+        } else if (*str_ptr1 == ' ') {
+            *str_ptr1 = '\0';
+            reverse(str_ptr0);
+            *str_ptr1 = ' ';
+            str_ptr0 = str_ptr1 + 1;
+        }                
+        str_ptr1++;
+    }
+}
+
+char* tokenize(char* str_or_null, const char* delims)
+{
+    static char* msg; 
+    char* ptr;
+
+    if (str_or_null != NULL) {
+        msg = str_or_null;
+    }
+    ptr = msg;
+ 
+    if (*msg == '\0') {
+        return NULL;
+    }    
+
+    while (*msg != '\0') {
+        const char* delims_ptr = delims; 
+        while (*delims_ptr != '\0') {
+            if (*msg == *delims_ptr) {
+                if (ptr == msg) {
+                    ptr++;
+                } else {
+                    *msg = '\0';
+                    msg++;
+                    goto label1;
+                }
+            }
+            delims_ptr++;
+        } 
+        msg++;
+    }
+label1:
+    return ptr;
+}
+
+char* reverse_tokenize(char* str_or_null, const char* delims)
+{
+    static char* msg; 
+    char* ptr;
+
+    if (str_or_null != NULL) {
+        msg = str_or_null;
+    }
+    ptr = msg;
+ 
+    if (*msg == '\0') {
+        return NULL;
+    }    
+
+    while (*msg != '\0') {
+        const char* delims_ptr = delims; 
+        while (*delims_ptr != '\0') {
+            if (*msg == *delims_ptr) {
+                if (ptr == msg) {
+                    ptr++;
+                } else {
+                    *msg = '\0';
+                    msg++;
+                    goto label1;
+                }
+            }
+            delims_ptr++;
+        } 
+        msg++;
+    }
+label1:
+    reverse(ptr);
+    return ptr;
+}
+
+static int length(const char * str) {
+    const char* ptr = str;
+    while (*ptr != '\0') {
+        ptr++;
+    }
+    return ptr - str;
+}
