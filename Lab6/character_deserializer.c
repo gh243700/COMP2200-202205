d5b93cea0ed88106016cd7b78ebdaa28d47a27db
JEL97 <gh243700@gmail.com>
2022-06-09 11:42:00 +0900

    Update character_deserializer.c

--- Lab6/character_deserializer.c
+++ Lab6/character_deserializer.c
@@ -61,22 +61,102 @@ int get_character(const char* filename, character_v3_t* out_character)
     return file_version;
 }
 
-void get_name(char* out, char* cp) 
+void get_character_v1(FILE* fp, character_v3_t* out_character) 
 {
-    
-    int i = 0;
-    while (*cp != '\0') {
-        if (i >= 50) {
-            break;
+    char buffer[256];     
+    char* attribute;
+    size_t index;
+    int i;
+    int magic_resistance;
+    fgets(buffer, 256, fp);
+
+    attribute = strtok(buffer, ",");
+
+    for (i = 0; i < 8; i++) {
+
+        index = 0;
+        
+        while (*attribute != '\0') {
+            if (*attribute == ':') {
+                *attribute = '\0';
+                break;
+            }
+            index++;
+            attribute++;
         }
         
-        if ((*cp >= 'A' && *cp <= 'Z') || (*cp >= 'a' && *cp <= 'z') || (*cp >= '0' && *cp <= '9') || *cp == '_') {
-            *out++ = *cp;
-            i++;
+        attribute -= index;
+
+        
+        if (strcmp(attribute, "id") == 0) {
+            char name[51] = "player_";
+            strcat(name, attribute + index + 1);
+            name[50] = '\0';
+            strncpy(out_character->name, name, 51);
+        } else if (strcmp(attribute, "lvl") == 0) {
+            sscanf(attribute + index + 1, "%d", &(out_character->level));
+        } else if (strcmp(attribute, "str") == 0) {
+            sscanf(attribute + index + 1, "%d", &(out_character->strength));
+        } else if (strcmp(attribute, "dex") == 0) {
+            sscanf(attribute + index + 1, "%d", &(out_character->dexterity));
+        } else if (strcmp(attribute, "intel") == 0) {
+            sscanf(attribute + index + 1, "%d", &(out_character->intelligence));
+        } else if (strcmp(attribute, "def") == 0) {
+            sscanf(attribute + index + 1, "%d", &(out_character->armour));
+        } else if (strcmp(attribute, "hp") == 0) {
+            sscanf(attribute + index + 1, "%d", &(out_character->health));
+        } else if (strcmp(attribute, "mp") == 0) {
+            sscanf(attribute + index + 1, "%d", &(out_character->mana));
         }
-        cp++;
+        attribute = strtok(NULL, ",");
     }
-    *out = '\0';
+    magic_resistance = out_character->armour / 4;
+    out_character->elemental_resistance.fire = magic_resistance / 3;
+    out_character->elemental_resistance.cold = magic_resistance / 3;
+    out_character->elemental_resistance.lightning = magic_resistance / 3;
+    out_character->minion_count = 0;
+    out_character->leadership = out_character->level / 10;
+    out_character->evasion = out_character->dexterity / 2;
+}
+
+
+void get_character_v2(FILE* fp, character_v3_t* out_character) 
+{
+    char buffer[512];
+    char name[128];
+    char* cp;
+    int magic_resistance;
+    fgets(buffer, 512, fp);
+    fgets(buffer, 512, fp);
+    
+    cp = strtok(buffer, ",");
+    get_name(name, cp);
+    name[50] = '\0';
+    strncpy(out_character->name, name, 51);
+    cp = strtok(NULL, ",");
+    sscanf(cp, "%d", &(out_character->level));
+    cp = strtok(NULL, ",");
+    sscanf(cp, "%d", &(out_character->strength));
+    cp = strtok(NULL, ",");
+    sscanf(cp, "%d", &(out_character->dexterity));
+    cp = strtok(NULL, ",");
+    sscanf(cp, "%d", &(out_character->intelligence));
+    cp = strtok(NULL, ",");
+    sscanf(cp, "%d", &(out_character->armour));
+    cp = strtok(NULL, ",");
+    sscanf(cp, "%d", &(out_character->evasion));
+    cp = strtok(NULL, ",");
+    sscanf(cp, "%d", &magic_resistance);
+    cp = strtok(NULL, ",");
+    sscanf(cp, "%d", &(out_character->health));
+    cp = strtok(NULL, ",");
+    sscanf(cp, "%d", &(out_character->mana));
+    
+    out_character->leadership = out_character->level / 10;
+    out_character->minion_count = 0;
+    out_character->elemental_resistance.fire = magic_resistance / 3;
+    out_character->elemental_resistance.cold = magic_resistance / 3;
+    out_character->elemental_resistance.lightning = magic_resistance / 3;
 }
 
 
@@ -159,102 +239,22 @@ void get_character_v3(FILE* fp, character_v3_t* out_character)
     
 }
 
-void get_character_v2(FILE* fp, character_v3_t* out_character) 
+void get_name(char* out, char* cp) 
 {
-    char buffer[512];
-    char name[128];
-    char* cp;
-    int magic_resistance;
-    fgets(buffer, 512, fp);
-    fgets(buffer, 512, fp);
     
-    cp = strtok(buffer, ",");
-    get_name(name, cp);
-    name[50] = '\0';
-    strncpy(out_character->name, name, 51);
-    cp = strtok(NULL, ",");
-    sscanf(cp, "%d", &(out_character->level));
-    cp = strtok(NULL, ",");
-    sscanf(cp, "%d", &(out_character->strength));
-    cp = strtok(NULL, ",");
-    sscanf(cp, "%d", &(out_character->dexterity));
-    cp = strtok(NULL, ",");
-    sscanf(cp, "%d", &(out_character->intelligence));
-    cp = strtok(NULL, ",");
-    sscanf(cp, "%d", &(out_character->armour));
-    cp = strtok(NULL, ",");
-    sscanf(cp, "%d", &(out_character->evasion));
-    cp = strtok(NULL, ",");
-    sscanf(cp, "%d", &magic_resistance);
-    cp = strtok(NULL, ",");
-    sscanf(cp, "%d", &(out_character->health));
-    cp = strtok(NULL, ",");
-    sscanf(cp, "%d", &(out_character->mana));
-    
-    out_character->leadership = out_character->level / 10;
-    out_character->minion_count = 0;
-    out_character->elemental_resistance.fire = magic_resistance / 3;
-    out_character->elemental_resistance.cold = magic_resistance / 3;
-    out_character->elemental_resistance.lightning = magic_resistance / 3;
-}
-
-void get_character_v1(FILE* fp, character_v3_t* out_character) 
-{
-    char buffer[256];     
-    char* attribute;
-    size_t index;
-    int i;
-    int magic_resistance;
-    fgets(buffer, 256, fp);
-
-    attribute = strtok(buffer, ",");
-
-    for (i = 0; i < 8; i++) {
-
-        index = 0;
-        
-        while (*attribute != '\0') {
-            if (*attribute == ':') {
-                *attribute = '\0';
-                break;
-            }
-            index++;
-            attribute++;
+    int i = 0;
+    while (*cp != '\0') {
+        if (i >= 50) {
+            break;
         }
         
-        attribute -= index;
-
-        
-        if (strcmp(attribute, "id") == 0) {
-            char name[51] = "player_";
-            strcat(name, attribute + index + 1);
-            name[50] = '\0';
-            strncpy(out_character->name, name, 51);
-        } else if (strcmp(attribute, "lvl") == 0) {
-            sscanf(attribute + index + 1, "%d", &(out_character->level));
-        } else if (strcmp(attribute, "str") == 0) {
-            sscanf(attribute + index + 1, "%d", &(out_character->strength));
-        } else if (strcmp(attribute, "dex") == 0) {
-            sscanf(attribute + index + 1, "%d", &(out_character->dexterity));
-        } else if (strcmp(attribute, "intel") == 0) {
-            sscanf(attribute + index + 1, "%d", &(out_character->intelligence));
-        } else if (strcmp(attribute, "def") == 0) {
-            sscanf(attribute + index + 1, "%d", &(out_character->armour));
-        } else if (strcmp(attribute, "hp") == 0) {
-            sscanf(attribute + index + 1, "%d", &(out_character->health));
-        } else if (strcmp(attribute, "mp") == 0) {
-            sscanf(attribute + index + 1, "%d", &(out_character->mana));
+        if ((*cp >= 'A' && *cp <= 'Z') || (*cp >= 'a' && *cp <= 'z') || (*cp >= '0' && *cp <= '9') || *cp == '_') {
+            *out++ = *cp;
+            i++;
         }
-        attribute = strtok(NULL, ",");
+        cp++;
     }
-    magic_resistance = out_character->armour / 4;
-    out_character->elemental_resistance.fire = magic_resistance / 3;
-    out_character->elemental_resistance.cold = magic_resistance / 3;
-    out_character->elemental_resistance.lightning = magic_resistance / 3;
-    out_character->minion_count = 0;
-    out_character->leadership = out_character->level / 10;
-    out_character->evasion = out_character->dexterity / 2;
+    *out = '\0';
 }
-
 
 
